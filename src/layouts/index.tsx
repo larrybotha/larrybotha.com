304223667f3e8ce703b0361dc7ee51060b8256c2
Larry Botha <larry@fixate.it>
2018-07-15 19:55:00 +0200

    chore(scss): add scss files

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,43 +1,24 @@
-import * as React from 'react'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
+import * as React from 'react';
+import Link from 'gatsby-link';
+import Helmet from 'react-helmet';
 
-import './index.css'
+import '../assets/scss/style.scss';
 
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
+  <div>
+    <div>
+      <h1>
+        <Link to="/">Gatsby</Link>
       </h1>
     </div>
   </div>
-)
+);
 
 interface DefaultLayoutProps extends React.HTMLProps<HTMLDivElement> {
   location: {
-    pathname: string
-  }
-  children: any
+    pathname: string;
+  };
+  children: any;
 }
 
 class DefaultLayout extends React.PureComponent<DefaultLayoutProps, void> {
@@ -47,8 +28,8 @@ class DefaultLayout extends React.PureComponent<DefaultLayoutProps, void> {
         <Helmet
           title="Gatsby Default Starter"
           meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
+            {name: 'description', content: 'Sample'},
+            {name: 'keywords', content: 'sample, something'},
           ]}
         />
         <Header />
@@ -63,8 +44,8 @@ class DefaultLayout extends React.PureComponent<DefaultLayoutProps, void> {
           {this.props.children()}
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default DefaultLayout
+export default DefaultLayout;
