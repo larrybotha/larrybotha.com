a36795412a1b588cf08ce0a089a4d85aeb9c17df
Larry Botha <larry@fixate.it>
2018-07-15 20:06:58 +0200

    chore(contentful): add contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Typescript Starter`,
@@ -9,6 +11,14 @@ module.exports = {
       resolve: `gatsby-plugin-sass`,
       options: {
         includePaths: [`${__dirname}/node_modules/normalize.css`],
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        host: `preview.contentful.com`,
       },
     },
   ],
