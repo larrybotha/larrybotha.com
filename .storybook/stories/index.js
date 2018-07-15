8607fe2b6a2ffce5d9326c8ff1235c3b614f5c3e
Larry Botha <larry@fixate.it>
2018-07-15 19:53:54 +0200

    chore(packages): add storybook

---
+++ .storybook/stories/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+import {action} from '@storybook/addon-actions';
+
+storiesOf('Button', module)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with some emoji', () => (
+    <button onClick={action('clicked')}>
+      <span role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </span>
+    </button>
+  ));
