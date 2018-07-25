09364b1619f0cb9bbaed266c895e09742ff5381f
Larry Botha <larry@fixate.it>
2018-07-25 09:15:12 +0200

    test(article): test title and content

--- __tests__/Article.test.tsx
+++ __tests__/Article.test.tsx
@@ -1,34 +1,99 @@
 import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import {cleanup, fireEvent, render} from 'react-testing-library';
 
 import Article from '../src/templates/Article';
 
-jest.mock('gatsby-link', () => {
-  const React = require('react');
+const container = document.createElement('div');
 
-  return <div />;
+jest.mock('gatsby-link', () => {
+  return {
+    default() {
+      const React = require('react');
+      return <div />;
+    },
+  };
 });
 
 describe('Article', () => {
-  test('-> dynamically loads rendered scripts', () => {
+  test('-> renders content', () => {
+    const html = `<div>foo</div>`;
     const data = {
       site: {
         siteMetadata: {
-          title: 'hey',
+          title: 'site title',
         },
       },
       contentfulArticle: {
         body: {
           childMarkdownRemark: {
-            html: `<script src="./foo.js"></script> `,
+            html,
           },
         },
       },
     };
-    const {container, getByTestId} = render(<Article data={data} />);
-    const headScripts = window.document.head.querySelector('script');
-    const scripts = headScripts ? [].slice.call(headScripts, 0) : 0;
+    const {container} = render(<Article data={data} />);
 
-    expect(scripts.length).toEqual(1);
+    expect(container.innerHTML).toContain(html);
+  });
+
+  test('-> renders title', done => {
+    const siteTitle = 'site title';
+    const postTitle = 'post title';
+    const data = {
+      site: {
+        siteMetadata: {
+          title: siteTitle,
+        },
+      },
+      contentfulArticle: {
+        title: postTitle,
+        body: {
+          childMarkdownRemark: {
+            html: '',
+          },
+        },
+      },
+    };
+
+    ReactDOM.render(<Article data={data} />, container);
+
+    requestAnimationFrame(() => {
+      expect(window.document.title).toContain(postTitle);
+      expect(window.document.title).toContain(siteTitle);
+      done();
+    });
+  });
+
+  test('-> dynamically loads rendered scripts', done => {
+    const data = {
+      site: {
+        siteMetadata: {
+          title: 'foo',
+        },
+      },
+      contentfulArticle: {
+        body: {
+          childMarkdownRemark: {
+            html: `
+              <div>
+                <script src="./foo.js"></script>
+              </div>
+            `,
+          },
+        },
+      },
+    };
+    const ArticleTemplate = ReactDOM.render(<Article data={data} />, container);
+    const root = render(<Article data={data} />);
+
+    requestAnimationFrame(() => {
+      const {container} = root;
+      const headScripts = window.document.querySelector('script');
+      const scripts = headScripts ? [].slice.call(headScripts, 0) : 0;
+
+      expect(scripts.length).toEqual(1);
+      done();
+    });
   });
 });
