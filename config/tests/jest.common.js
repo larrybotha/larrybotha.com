94aa0ef6940bc1a74e2c703fa5bc089d5f6ff20e
Larry Botha <larry@fixate.it>
2018-12-20 22:39:47 +0200

    chore(test configs): update

--- config/tests/jest.common.js
+++ config/tests/jest.common.js
@@ -5,19 +5,34 @@ module.exports = {
 
   testEnvironment: 'jest-environment-jsdom',
 
-  moduleFileExtensions: ["js", "jsx", "json", "ts", "tsx"],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
 
   testMatch: ['<rootDir>/src/**/*.test.(js|ts|tsx)'],
+
+  moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': require.resolve(
+      './fileMock.js',
+    ),
+  },
+
+  testPathIgnorePatterns: ['node_modules', '.cache'],
+
+  transformIgnorePatterns: ['node_modules/(?!(gatsby)/)'],
 
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
 
+  globals: {
+    __PATH_PREFIX__: '',
+  },
+
   setupFiles: ['<rootDir>/config/tests/jest-setup.js'],
 
   watchPlugins: [
-    // 'jest-watch-typeahead/filename',
-    // 'jest-watch-typeahead/testname',
-    // 'jest-watch-select-projects',
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+    'jest-watch-select-projects',
   ],
 };
