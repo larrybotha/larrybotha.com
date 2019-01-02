2b0b59917bde419714a845ca10caad9cadb274c6
Larry Botha <larry@fixate.it>
2019-01-02 10:25:36 +0200

    fix template filename

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,8 @@ exports.createPages = ({graphql, actions}) => {
   const {createPage} = actions;
 
   return new Promise((resolve, reject) => {
-    const article = path.resolve('./src/templates/Article.tsx');
+    const article = path.resolve('./src/templates/article.tsx');
+
     resolve(
       graphql(
         `
@@ -22,17 +23,17 @@ exports.createPages = ({graphql, actions}) => {
       ).then(result => {
         if (result.errors) {
           console.log(result.errors);
+
           reject(result.errors);
         }
 
         const posts = result.data.allContentfulArticle.edges;
+
         posts.forEach((post, index) => {
           createPage({
-            path: `/articles/${post.node.slug}/`,
             component: article,
-            context: {
-              slug: post.node.slug,
-            },
+            context: {slug: post.node.slug},
+            path: `/articles/${post.node.slug}/`,
           });
         });
       }),
