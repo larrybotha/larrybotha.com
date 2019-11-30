5ee4fd92cea7cd13f9405af29bab5a31aeb2ce4d
Larry Botha <larry@fixate.it>
2019-11-30 07:58:25 -0500

    delint

--- packages/project-food/src/routes/index.svelte
+++ packages/project-food/src/routes/index.svelte
@@ -6,12 +6,6 @@
 		margin: 0 0 0.5em 0;
 	}
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
 	p {
 		margin: 1em auto;
 	}
@@ -211,10 +205,7 @@
 {/if}
 
 {#each cats as cat, i (cat.id)}
-  <div
-    in:receive="{{key: cat.id}}"
-    out:send="{{key: cat.id}}"
-  >
+  <div>
     <h2>{cat.title}</h2>
 
     {#each cat.groups as foodGroup, i (foodGroup.id)}
