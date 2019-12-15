5b7a9a64a176fbfecb6d428a8f8ba9d6af17de7b
Larry Botha <larry@fixate.it>
2019-12-15 18:08:45 -0500

    ensure new actors are initialised with preceding actor's state

--- packages/json-editor/src/machines/value.js
+++ packages/json-editor/src/machines/value.js
@@ -1,11 +1,34 @@
 import {assign, Machine, send, spawn} from 'xstate';
 
+const getTransitionTypeFromStateValue = state => {
+  switch (state) {
+    case 'primitive':
+      return 'SELECT_PRIMITIVE';
+    case 'primitive.boolean':
+      return 'SELECT_BOOLEAN';
+    case 'primitive.string':
+      return 'SELECT_STRING';
+    case 'primitive.number':
+      return 'SELECT_NUMBER';
+    case 'primitive.null':
+      return 'SELECT_NULL';
+    case 'primitive.undefined':
+      return 'SELECT_UNDEFINED';
+    case 'object':
+      return 'SELECT_OBJECT';
+    case 'array':
+      return 'SELECT_ARRAY';
+    default:
+      return;
+  }
+};
+
 const valueMachine = Machine(
   {
-    initial: 'unknown',
+    initial: 'primitive',
 
     context: {
-      name: undefined,
+      key: undefined,
       value: undefined,
       values: [],
     },
@@ -14,8 +37,8 @@ const valueMachine = Machine(
       SELECT_ARRAY: 'array',
       SELECT_OBJECT: 'object',
       SELECT_PRIMITIVE: 'primitive',
-      SET_PROPERTY_NAME: {
-        actions: ['setPropertyName'],
+      SET_PROPERTY_KEY: {
+        actions: ['setPropertyKey'],
       },
     },
 
@@ -26,7 +49,7 @@ const valueMachine = Machine(
         on: {
           ADD_ACTOR: {
             target: '.',
-            actions: ['spawnActor'],
+            actions: ['spawnActor', 'setNewActorState'],
           },
           REMOVE_ACTOR: {
             target: '.',
@@ -37,17 +60,20 @@ const valueMachine = Machine(
 
       object: {
         on: {
-          SEND_PROPERTY_NAME: {
+          SEND_PROPERTY_KEY: {
+            actions: ['sendPropertyKey'],
+            external: true,
             target: '.',
-            actions: ['sendPropertyName'],
           },
           ADD_ACTOR: {
+            actions: ['spawnActor', 'setNewActorState'],
+            external: true,
             target: '.',
-            actions: ['spawnActor'],
           },
           REMOVE_ACTOR: {
-            target: '.',
             actions: ['removeActor'],
+            external: true,
+            target: '.',
           },
         },
       },
@@ -65,6 +91,8 @@ const valueMachine = Machine(
 
         states: {
           string: {
+            entry: 'setDefaultStringValue',
+
             on: {
               SET_VALUE: {
                 actions: 'setValue',
@@ -73,6 +101,8 @@ const valueMachine = Machine(
           },
 
           number: {
+            entry: 'setDefaultNumberValue',
+
             on: {
               SET_VALUE: {
                 actions: 'setValue',
@@ -101,20 +131,13 @@ const valueMachine = Machine(
   },
   {
     actions: {
-      setPropertyName: assign({
-        name: (_, {data}, x, z) => {
-          return data;
-        },
+      setPropertyKey: assign({
+        key: (_, {data}) => data,
       }),
 
-      sendPropertyName: send(
-        (_, {data}) => {
-          return {type: 'SET_PROPERTY_NAME', data};
-        },
-        {
-          to: ({values}, {refId}) => values.find(({ref}) => ref.id === refId).ref,
-        },
-      ),
+      sendPropertyKey: send((_, {data}) => ({type: 'SET_PROPERTY_KEY', data}), {
+        to: ({values}, {refId}) => values.find(({ref}) => ref.id === refId).ref,
+      }),
 
       setValue: assign({value: (_, {data}) => data}),
 
@@ -122,18 +145,34 @@ const valueMachine = Machine(
         value: (_, {data}) => Boolean(data),
       }),
 
+      setDefaultStringValue: assign({value: ''}),
+
+      setDefaultNumberValue: assign({value: 0}),
+
       setNullValue: assign({value: null}),
 
       setUndefinedValue: assign({value: undefined}),
 
       spawnActor: assign({
-        values: ({values}) => [
-          ...values,
-          {
-            ref: spawn(valueMachine),
-          },
-        ],
+        values: ({values}) => [...values, {ref: spawn(valueMachine, {sync: true})}],
       }),
+
+      setNewActorState: ctx => {
+        const {values} = ctx;
+
+        /**
+         * set new actor to be in same state as previous actor
+         */
+        if (values.length > 1) {
+          const [prevActor, newActor] = values.slice(-2);
+          const events = prevActor.ref.state
+            .toStrings()
+            .map(getTransitionTypeFromStateValue)
+            .map(type => ({type}));
+
+          newActor.ref.send(events);
+        }
+      },
 
       removeActor: assign({
         values: ({values}, {data}) => values.filter(({ref}) => ref.id !== data.id),
