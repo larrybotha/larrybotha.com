1132398ffe168fac1883dc69bc10412b6a65e863
Larry Botha <larry@fixate.it>
2019-12-17 22:05:17 -0500

    add hover styles to buttons

--- packages/json-editor/src/components/Button.svelte
+++ packages/json-editor/src/components/Button.svelte
@@ -1,13 +1,20 @@
+<script>
+  export let className;
+  export const classModifiers = {
+    round: 'btn--round',
+  };
+</script>
+
 <style>
-  button {
+  .btn {
     background-image: none; /* remove gradient from FF on mobile */
-    background-color: #333;
+    background-color: #aaa;
     border: 0;
     border-radius: 2em;
     color: #fff;
     cursor: pointer;
     display: inline-block;
-    height: auto; /* ensure heights set on inputs do not affect submit buttons */
+    height: auto; /* ensure heights set on inputs do not affect submit .btns */
     line-height: normal; /* let's make all browsers match FF's !important */
     margin: 0;
     padding: 0.25em 0.5em;
@@ -16,14 +23,27 @@
     vertical-align: middle;
     white-space: nowrap;
     width: auto;
+    transition: all 0.15s ease-in-out;
   }
 
-  button:active,
-  button:focus {
+  .btn:hover {
+    background-color: #666;
+  }
+
+  .btn:active {
+    background-color: #aaa;
+  }
+
+  .btn:active,
+  .btn:focus {
     outline: none;
+  }
+
+  .btn--round {
+    border-radius: 50%;
   }
 </style>
 
-<button on:click>
+<button class={`btn ${className}`} on:click>
   <slot />
 </button>
