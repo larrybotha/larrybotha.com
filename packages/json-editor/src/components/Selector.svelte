8b727d5db5354de8fb7dbc6437de1f133aac9318
Larry Botha <larry@fixate.it>
2019-12-20 09:08:42 -0500

    animate items in and out

--- packages/json-editor/src/components/Selector.svelte
+++ packages/json-editor/src/components/Selector.svelte
@@ -1,18 +1,46 @@
 <script>
+  import {spring} from 'svelte/motion';
+  import {fade} from 'svelte/transition';
+
   import Button from './Button.svelte';
   export let buttonContent = '+';
+  export let items = [];
+  let itemCoords = spring(items.map(() => ({x: 0, y: 0})), {
+    stiffness: 0.1,
+    damping: 0.25,
+  });
   let isActive = false;
 
-  const handleClick = ev => {
+  const setCoords = async () => {
+    return await itemCoords.set(
+      items.map((_, i) => {
+        const {length} = items;
+        const pos = Math.floor(i - length / 4);
+        const angle = (pos * (Math.PI * 0.8)) / length;
+        const radius = items.length * 12;
+        const x = radius * Math.cos(angle);
+        const y = radius * Math.sin(angle);
+
+        return {x, y};
+      }),
+    );
+  };
+
+  const resetCoords = async () => await itemCoords.set(items.map(() => ({x: 0, y: 0})));
+
+  const handleClick = async ev => {
     ev.stopPropagation();
 
+    setCoords();
     isActive = !isActive;
   };
-  const deactivateSelector = () => {
+  const deactivateSelector = async () => {
+    await resetCoords();
     isActive = false;
   };
-  const closeSelector = () => {
+  const closeSelector = async () => {
     if (isActive) {
+      await resetCoords();
       isActive = false;
     }
   };
@@ -24,14 +52,11 @@
     display: inline-block;
   }
 
-  .selector__btn {
-  }
-
   .selector__content {
     position: absolute;
-    top: 100%;
-    left: 100%;
-    transform: translateY(-50%);
+    top: 0%;
+    left: 0%;
+    transform: translate3d(-50%, -50%, 0);
   }
 
   .selector__content[aria-hidden='true'] {
@@ -44,14 +69,31 @@
     opacity: 1;
     z-index: 5;
   }
+
+  .selector__item {
+    display: inline-block;
+    position: absolute;
+    left: 100%;
+  }
 </style>
 
 <svelte:body on:click={isActive ? closeSelector : () => {}} />
 
 <div class="selector">
-  <Button class="selector__btn" on:click={handleClick}>{buttonContent}</Button>
+  <Button class="selector__btn" on:click={isActive ? deactivateSelector : handleClick}>
+    {buttonContent}
+  </Button>
 
-  <div class="selector__content" aria-hidden={!isActive}>
-    <slot />
-  </div>
+  {#if isActive}
+    <div class="selector__content" aria-hidden={!isActive} in:fade out:fade>
+      {#each items as item, i}
+        <span
+          class="selector__item"
+          key={i}
+          style="transform: translate3d( {$itemCoords[i].x}px, {$itemCoords[i].y}px, 0);">
+          <slot {item} />
+        </span>
+      {/each}
+    </div>
+  {/if}
 </div>
