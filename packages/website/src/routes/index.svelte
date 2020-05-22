5321de1188a24589e28669cb71c9efbf2c6475ab
Larry Botha <larry@fixate.it>
2020-05-21 23:57:26 -0500

    import styles

--- packages/website/src/routes/index.svelte
+++ packages/website/src/routes/index.svelte
@@ -31,40 +31,8 @@
   export let articles = [];
 </script>
 
-<style>
-  h1,
-  figure,
-  p {
-    text-align: center;
-    margin: 0 auto;
-  }
-
-  h1 {
-    font-size: 2.8em;
-    text-transform: uppercase;
-    font-weight: 700;
-    margin: 0 0 0.5em 0;
-  }
-
-  figure {
-    margin: 0 0 1em 0;
-  }
-
-  img {
-    width: 100%;
-    max-width: 400px;
-    margin: 0 0 1em 0;
-  }
-
-  p {
-    margin: 1em auto;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      font-size: 4em;
-    }
-  }
+<style lang="scss" global>
+  @import '../scss/style.scss';
 </style>
 
 <svelte:head>
