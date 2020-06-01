0984deb314fdd7dfad09d83214fbab1fbd372d33
Larry Botha <larry@fixate.it>
2020-05-31 21:58:42 -0500

    add seo fields to blog layout

--- src/routes/blog/_blog-layout.svelte
+++ src/routes/blog/_blog-layout.svelte
@@ -1,12 +1,15 @@
-<script context="module">
-  export async function preload(page, session) {
-    debugger;
-  }
-</script>
-
 <script>
   export let title;
+  export let seoTitle;
+  export let seoDescription;
+
+  const seoTitleSuffixed = [seoTitle, 'Larry Botha'].join(' | ');
 </script>
+
+<svelte:head>
+  <title>{seoTitleSuffixed}</title>
+  <meta name="description" content={seoDescription} />
+</svelte:head>
 
 <div class="wrap">
   <h1>{title}</h1>
