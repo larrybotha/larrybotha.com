ed7f026d1e819e9284aa8d7249eacb5ba82b1c7f
Larry Botha <larry@fixate.it>
2020-06-24 23:59:25 -0500

    add notes

---
+++ src/routes/notes/_posts.js
@@ -0,0 +1,46 @@
+const {resolve} = require('path');
+const {readdir, readFile} = require('fs').promises;
+const {transform} = require('mdsvex');
+
+async function getFiles(dir) {
+  const dirents = await readdir(dir, {withFileTypes: true});
+  const files = await Promise.all(
+    dirents.map(dirent => {
+      const res = resolve(dir, dirent.name);
+
+      return dirent.isDirectory() ? getFiles(res) : res;
+    }),
+  );
+
+  return Array.prototype.concat(...files);
+}
+
+async function getPosts() {
+  const dir = resolve(process.cwd(), 'src/routes/notes');
+  const files = await getFiles(dir);
+  const indexFiles = files.filter(filename => {
+    return filename.endsWith('index.svx');
+  });
+  const parser = transform();
+  const posts = await Promise.all(
+    indexFiles.map(async filename => {
+      const contents = await readFile(filename, 'utf-8');
+      const parsed = await parser.process({contents, filename});
+      const frontMatter = parsed.data.fm || {};
+      const slug = filename.split('/').slice(-2)[0];
+
+      return {
+        html: parsed.contents,
+        slug,
+        ...frontMatter,
+      };
+    }),
+  );
+  const sortedPosts = posts.sort((a, b) =>
+    a.publishDate > b.publishDate ? 1 : -1,
+  );
+
+  return sortedPosts;
+}
+
+export {getPosts};
---
+++ src/routes/notes/index.svelte
@@ -0,0 +1,35 @@
+<script context="module">
+  import {getPosts} from 'src/stores/posts';
+
+  export async function preload() {
+    const posts = await getPosts(this);
+
+    return {posts};
+  }
+</script>
+
+<script>
+  export let posts;
+</script>
+
+<style>
+  ul {
+    margin: 0 0 1em 0;
+    line-height: 1.5;
+  }
+</style>
+
+<svelte:head>
+  <title>Blog | Larry Botha</title>
+  <meta name="description" content="Larry Botha's blog" />
+</svelte:head>
+
+<h1>Recent posts</h1>
+
+<ul>
+  {#each posts as post}
+    <li>
+      <a href="blog/{post.slug}">{post.title}</a>
+    </li>
+  {/each}
+</ul>
