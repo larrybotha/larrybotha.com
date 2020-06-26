bb8226cae9b23c9e8acdf4e53602478899900471
Larry Botha <larry@fixate.it>
2020-06-25 22:28:32 -0500

    update 404 page - closes #19

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -1,40 +1,28 @@
 <script>
-	export let status;
-	export let error;
+  export let status;
+  export let error;
 
-	const dev = process.env.NODE_ENV === 'development';
+  const dev = process.env.NODE_ENV === 'development';
 </script>
 
-<style>
-	h1, p {
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
-
 <svelte:head>
-	<title>{status}</title>
+  <title>{status}</title>
 </svelte:head>
 
-<h1>{status}</h1>
+<div class="wrap">
+  <div class="island u-text--center">
+    {#if status === 404}
+      <h1>Page not found</h1>
 
-<p>{error.message}</p>
+      <p>Doh! Somehow you've ended up on a page that doesn't exist!</p>
+    {:else}
+      <h1>{status}</h1>
 
-{#if dev && error.stack}
-	<pre>{error.stack}</pre>
-{/if}
+      <p>{error.message}</p>
+    {/if}
+
+    {#if dev && error.stack}
+      <pre>{error.stack}</pre>
+    {/if}
+  </div>
+</div>
