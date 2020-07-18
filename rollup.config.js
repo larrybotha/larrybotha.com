d3497c6673ba9a7b58259070872f0a9def216ef5
Larry Botha <larry@fixate.it>
2020-07-18 00:46:50 -0500

    try with deprecated babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import path from 'path';
 import alias from '@rollup/plugin-alias';
-import babel from '@rollup/plugin-babel';
+import babel from 'rollup-plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import config from 'sapper/config/rollup.js';
 import replace from '@rollup/plugin-replace';
